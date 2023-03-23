e5dec61f61656c80c7d2d88054bca855b75a825e
Martin Jacko <xjacko05@stud.fit.vutbr.cz>
2023-03-23 22:59:41 +0100

    parsing works

--- main.hs
+++ main.hs
@@ -1,18 +1,27 @@
 import System.IO
 import System.Environment
-import Data.Typeable
+--import System.Random
 
-data Point = Point { x :: Integer, y :: Integer } deriving (Read)
+data Point = Point { x :: Integer, y :: Integer } deriving (Read, Eq)
 data Curve = Curve { p :: Integer
                    , a :: Integer
                    , b :: Integer
                    , g :: Point
                    , n :: Integer
                    , h :: Integer
-                   } deriving (Read)
+                   }
+data Key = Key { d :: Integer, q :: Integer }
+data Signature = Signature { r :: Integer, s :: Integer }
+type Hash = Integer
 
 instance Show Point where
     show (Point x y) = "Point {\nx: " ++ show x ++ "\ny: " ++ show y ++ "\n}"
+
+instance Show Key where
+    show (Key d q) = "Key {\nd: " ++ show d ++ "\nQ: " ++ show q ++ "\n}"
+
+instance Show Signature where
+    show (Signature r s) = "Signature {\nr: " ++ show r ++ "\ns: " ++ show s ++ "\n}"
 
 instance Show Curve where
     show (Curve p a b g n h) = "Curve {\np: " ++ show p ++ "\na: " ++ show a ++ "\nb: " ++ show b ++ "\ng: " ++ show g ++ "\nn: " ++ show n ++ "\nh: " ++ show h ++ "\n}\n"
@@ -22,23 +31,47 @@ main = do
     args <- getArgs
     let (mode, file) = parseArgs args
     input <- if (file=="") then getContents else readFile file
-    let a = read input :: Curve
-    let c = Curve {p= 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F
-                  ,a= 0
-                  ,b= 7
-                  ,g= Point {
-                    x= 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798
-                   ,y= 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798
-                   }
-                  ,n= 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
-                  ,h= 1
-                  }
-    let k = map repl input
-    let u = foldl insertComma [] k
-    putStr (u)
-    putStr (show c)
-    putStr (show $ p a)
+    let curve = parseCurve input
+    let key = parseKey input
+    let signature = parseSignature input
+    let hash = parseHash input
+    --putStr (u)
+    --putStr (show c)
+    --putStrLn (show (getParam "y" (words input)))
+    case mode of "-i" -> putStrLn (show curve)
+
+    --putStrLn (show signature)
+    putStrLn (show $ addPoints (Point 13 16) (Point 0 0) 1 23)
+    putStrLn (show $ mulPoint (Point 13 16) 158158158 1 23)
     return ()
+
+
+getParam :: String -> [String] -> Integer
+getParam param arr =  read (head $ tail (dropWhile (/=(param ++ ":")) arr)) :: Integer
+
+parseCurve :: String -> Curve
+parseCurve input = Curve { p= getParam "p" (words input),
+                            a= getParam "a" (words input),
+                            b= getParam "b" (words input),
+                            g= Point {
+                                x= getParam "x" (words input),
+                                y= getParam "y" (words input)
+                            },
+                            n= getParam "n" (words input),
+                            h= getParam "h" (words input)
+                        }
+
+parseKey :: String -> Key
+parseKey input = Key {  d= getParam "d" (words input),
+                        q= getParam "Q" (words input)
+                    }
+
+parseSignature :: String -> Signature
+parseSignature input = Signature {  r= getParam "r" (words input),
+                                    s= getParam "s" (words input)
+                                }
+parseHash :: String -> Hash
+parseHash input = getParam "Hash" (words input)
 
 
 parseArgs :: [String] -> (String, String)
@@ -46,16 +79,27 @@ parseArgs [x] = (x, "")
 parseArgs [x,y] = (x, y)
 parseArgs _ = error "Agument parsing failed."
 
-repl :: Char -> Char
-repl ':' = '='
-repl c = c
+getModInverse :: Integer -> Integer -> Integer
+getModInverse value modulo = head [x | x <- [0..(modulo - 1)], mod (value * x) modulo == 1]
 
-insertComma :: [Char] -> Char -> String
-insertComma acc c
-    | c == 'a' = acc ++ ",a"
-    | c == 'b' = acc ++ ",b"
-    | c == 'g' = acc ++ ",g"
-    | c == 'y' = acc ++ ",y"
-    | c == 'n' = acc ++ ",n"
-    | c == 'h' = acc ++ ",h"
-    | otherwise = acc ++ [c]
+addPoints' :: Point -> Point -> Integer -> Integer -> Point
+addPoints' p q a modulo = Point coorX coorY
+        where
+    s
+        | x p == x q && y p == y q = ((3*(x p)^2 + a) * (getModInverse (2*y p) modulo)) `mod` modulo
+        | otherwise = ((y p - y q) * (getModInverse (x p - x q) modulo)) `mod` modulo
+    coorX = mod (s^2 - (x p) - (x q)) modulo
+    coorY = mod (s * (x p - coorX) - y p) modulo
+
+addPoints :: Point -> Point -> Integer -> Integer -> Point
+addPoints p q a modulo
+    | x p == y p && y p == 0 = q
+    | x q == y q && y q == 0 = p
+    | x p == x q && y p /= y q = Point 0 0
+    | x p == x q && y p == y q && y p == 0 = Point 0 0
+    | otherwise = addPoints' p q a modulo
+
+mulPoint :: Point -> Integer -> Integer -> Integer -> Point
+mulPoint p times a modulo
+    | times == 1 = p
+    | otherwise = addPoints (mulPoint p (times-1) a modulo) p a modulo
